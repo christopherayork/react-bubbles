f6be5270d7bc603a6e6837d2a3ab7bfa17034f46
Chris York <christopherayork1992@gmail.com>
2019-08-23 11:47:57 -0400

    MVP Done

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "./axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -21,10 +21,26 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth().put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        console.log(res);
+        setEditing(false);
+        let updatedColors = colors.map(c => {
+          if(c.id === colorToEdit) return colorToEdit;
+          else return c;
+        });
+        updateColors(updatedColors);
+      })
+      .catch(e => console.log(e));
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth().delete(`http://localhost:5000/api/colors/${color.id}`)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(e => console.log(e));
   };
 
   return (
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [values, setValues] = useState({username: '', password: ''});
+
+  const updateValue = e => {
+    setValues({...values, [e.target.name]: e.target.value});
+  };
+
+  const submit = e => {
+    e.preventDefault();
+    axios.post('http://localhost:5000/api/login', values)
+      .then(res => {
+        console.log(res.data);
+        localStorage.setItem('userToken', res.data.payload);
+        props.history.push('/bubble');
+      })
+      .catch(e => console.log(e));
+  };
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div>
+      <form name='Login'>
+        <input type='text' name='username' placeholder='Username' value={values.username} onChange={updateValue} />
+        <input type='password' name='password' placeholder='Password' value={values.password} onChange={updateValue} />
+        <button onClick={submit}>Login</button>
+      </form>
+    </div>
   );
 };
 
